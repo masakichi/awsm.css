c9cbd7b6f715b39e768cb1ececca5c48551a7eb1
igoradamenko <mail@igoradamenko.com>
2019-01-24 16:25:33 +0200

    Improve PostCSS plugin to work with themes.

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var fs = require('fs');
 var path = require('path');
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-awsm-css', function () {
+module.exports = postcss.plugin('postcss-awsm-css', function (options) {
+  options = options || {};
+  var theme = options.theme || 'white';
+
   return function(css) {
     return new Promise(function(resolve) {
-      var filepath = path.join(__dirname, 'dist', 'awsm.min.css');
+      var filepath = path.join(__dirname, 'dist', 'awsm_theme_' + theme + '.min.css');
       var filecontent = fs.readFileSync(filepath, 'utf8');
 
       css.prepend(filecontent);
