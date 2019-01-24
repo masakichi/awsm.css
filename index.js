b1a13faf8e77b0ab30a0575f9aac698f5f6d41c7
igoradamenko <mail@igoradamenko.com>
2019-01-24 18:50:50 +0200

    Add support of sealing styles to PostCSS plugin.

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-awsm-css', function (options) {
   options = options || {};
   var theme = options.theme || 'white';
+  var sealed = options.sealed || false;
+
+  var filenameThemed = theme ? ('awsm_theme_' + theme) : 'awsm';
+  var filenameSealed = sealed ? [filenameThemed, 'sealed'].join(theme ? '._' : '_') : filenameThemed;
+  var filename = filenameSealed + '.min.css';
 
   return function(css) {
     return new Promise(function(resolve) {
-      var filepath = path.join(__dirname, 'dist', 'awsm_theme_' + theme + '.min.css');
+      var filepath = path.join(__dirname, 'dist', filename);
       var filecontent = fs.readFileSync(filepath, 'utf8');
 
       css.prepend(filecontent);
